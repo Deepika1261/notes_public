e6aef6a86e61683a7fab77fbe2f8d4e0a82f33b2
Deepika1261 <57223834+Deepika1261@users.noreply.github.com>
2022-07-21 20:04:30 +0530

    notes app with vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,37 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<nav>
+  <v-toolbar flat app>
+    <v-app-bar-nav-icon @click="drawer = !drawer" class="grey--text"></v-app-bar-nav-icon>
+    <v-toolbar-title class="display-2 grey--text">
+      <span class="font-weight-light">Add</span>
+      <span>Notes</span>
+    </v-toolbar-title>
+  </v-toolbar>
+  <v-navigation-drawer width="500" app v-model="drawer" class="indigo lighten-4">
+  <div></div>
+  <div></div>
+  <hr />
+  <form>
+    <v-text-field v-model="note_heading" label="HEADING" required></v-text-field>
+    <v-text-field v-model="note" label="BODY" required></v-text-field>
+    <v-btn class="mr-4">submit</v-btn>
+    <v-btn >clear</v-btn>
+  </form>
+  </v-navigation-drawer>
+</nav>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data(){
+      return{
+        note:"",
+        note_heading:"",
+        drawer:false,
+      }
+    }
+  }
+</script>
---
+++ src/components/ViewNotes.vue
@@ -0,0 +1,52 @@
+<template>
+<v-container class="my-5">
+  <v-card flat v-for="note in notes" :key="note.id">
+    <v-layout row wrap :class="`pa-2 note ${notes.is_favourite}`">
+      <v-flex xs12 md6>
+        <div class="caption grey--text text-darken-3">{{note.note_heading}}</div>
+        <div>{{ note.note }}</div>
+      </v-flex>
+      <v-flex xs6 sm4 md2>
+        <div class="caption grey--text">Edit</div>
+        <v-btn class="ma-2" outlined large fab color="indigo">
+            <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+      </v-flex>
+      <v-flex xs6 sm4 md2>
+        <div class="caption grey--text">Delete</div>
+        <v-btn class="mx-2" fab dark small color="primary">
+            <v-icon dark >X</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+  </v-card>
+</v-container>
+</template>
+<script>
+export default {
+  name: "ViewNote",
+  data() {
+    return {
+      notes: [
+        { id: 1, is_favourite:false, note_heading: 'Book Writing', note: 'Lorem ipsum dolor sit amet consectetur adipisicinjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjg elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { id: 2, is_favourite:true, note_heading: 'Special holidays', note: 'North East Asia, Goa, Paris, Turkey'},
+        { id: 3, is_favourite:false, note_heading: 'Food rich in vitamin D', note: 'Eggs, Fish, Spinach, Beetroot dried nuts'},
+        { id: 4, is_favourite:true, note_heading: 'Hockey match', note: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+</style>
